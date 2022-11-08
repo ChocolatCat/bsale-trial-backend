714d31c72a7f8d15461f2509a3d02675e9f60fee
ChocolatCat <chocolatcat@protonmail.com>
2022-11-08 18:00:43 -0300

    Added pagination support

--- routes/products.js
+++ routes/products.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-//Obtener lista de productos
+//Obtener lista de productos y/o paginados
 router.get('/', function(req, res){
-    let sql = `SELECT id, name, price, discount, category,
+    let page = req.query.page  || 0;
+    let sql = page > 0 ? `SELECT id, name, price, discount, category,
         IFNULL(url_image, '') url_image
+    FROM product ORDER BY category LIMIT ${(page-1) * 9}, 9` : `SELECT id, name, price, discount, category,
+    IFNULL(url_image, '') url_image
     FROM product ORDER BY category`;
     db.query(sql, function(err, data, fields){
         if(err){
@@ -28,9 +31,9 @@ router.get('/search', function(req, res){
     `SELECT id, name, price, discount, category,
     IFNULL(url_image, '') url_image
     FROM product 
-    WHERE name LIKE %?%
+    WHERE name LIKE '%${search}%'
     ORDER BY category`;
-    db.query(sql, [search], function(err, data, fields){
+    db.query(sql, function(err, data, fields){
         if(err){
             console.log(err);
         }
